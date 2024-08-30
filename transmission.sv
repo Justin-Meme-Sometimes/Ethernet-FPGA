325ddd010e1cd80520ef6b6e48116d14ee932dcf
Justin <69047638+Justin-Meme-Sometimes@users.noreply.github.com>
2024-08-30 14:51:14 -0400

    created fsm

--- transmission.sv
+++ transmission.sv
@@ -13,6 +13,54 @@ module transmit
 endmodule;
 
 module transmit_fsm
+     enum {PREAMBLE,SFD,MAC_DEST,MAC_SRC,ETHER_TYPE,PAYLOAD,CRC32} 
+     current_state, next_state;
 
+     always_ff @(posedge clock, negedge reset_n) begin
+        if(!reset_n) begin
+            current_state <= INIT;
+        end
+        else begin
+            current_state <= next_state;
+        end
+     end
+
+    always_comb begin
+        PREAMBLE: begin
+            if(preamable_complete) begin
+                next_state = SFD;
+            end
+        end
+        SFD: begin
+            if(sfd_complete) begin
+                next_state = MAC_DEST;
+            end
+        end
+        MAC_DEST: begin
+            if(mac_dest_complete) begin
+                next_state = MAC_SRC;
+            end
+        end
+        MAC_SRC: begin
+            if(mac_src_complete) begin
+                next_state = ETHER_TYPE;
+            end
+        end
+        ETHER_TYPE: begin
+            if(ether_type_complete) begin
+                next_state = PAYLOAD;
+            end
+        end
+        PAYLOAD: begin
+            if(payload_complete) begin
+                next_state = CRC32;
+            end
+        end
+        CRC32: begin
+            if(crc32) begin
+                next_state = PREAMBLE;
+            end
+        end
+    end
 
 endmodule
